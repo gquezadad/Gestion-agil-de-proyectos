3eb81f7bcab4ff88c7e985b25b6c186e8e69afeb
unknown <di.henriquezp@alumnos.duoc.cl>
2019-05-06 13:45:35 -0400

    borrado

--- Libroteca/Examen/Sistema/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Usuario,Producto,Tienda,Lista
-
-# Register your models here.
-# Crea el espacio para los objetos
-admin.site.register(Usuario)
-admin.site.register(Producto)
-admin.site.register(Tienda)
-admin.site.register(Lista)
